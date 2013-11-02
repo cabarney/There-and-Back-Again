bceefe43082cae8aa602491e7d9aa4866c14f2e1
Adam Barney <adam@Heisenberg.local>
2013-11-02 00:46:04 -0500

    Breeze

--- demos/Todo/Controllers/TodosController.cs
+++ demos/Todo/Controllers/TodosController.cs
@@ -1,5 +1,5 @@
-namespace Todo.Controllers {
-    using System;
+namespace Todo.Controllers
+{
     using System.Linq;
     using System.Web.Http;
     using Breeze.WebApi;
@@ -8,37 +8,30 @@ namespace Todo.Controllers {
     using Newtonsoft.Json.Linq;
 
     [BreezeController]
-    public class TodosController : ApiController {
-
-        static readonly TimeSpan RefreshRate = TimeSpan.FromMinutes(60);
-        private static readonly object Locker = new object();
-        static DateTime _lastRefresh = DateTime.Now; // will first clear db at Now + "RefreshRate" 
-        // static DateTime lastRefresh = DateTime.MinValue; // will clear when server starts
-
-        readonly EFContextProvider<TodosContext> _contextProvider =
+    public class TodosController : ApiController
+    {
+        private readonly EFContextProvider<TodosContext> _contextProvider =
             new EFContextProvider<TodosContext>();
-
-        public TodosController()
-        {
-            PeriodicReset();
-        }
 
         // ~/breeze/todos/Metadata 
         [HttpGet]
-        public string Metadata() {
+        public string Metadata()
+        {
             return _contextProvider.Metadata();
         }
 
         // ~/breeze/todos/Todos
         // ~/breeze/todos/Todos?$filter=IsArchived eq false&$orderby=CreatedAt 
         [HttpGet]
-        public IQueryable<TodoItem> Todos() {
+        public IQueryable<TodoItem> Todos()
+        {
             return _contextProvider.Context.Todos;
         }
 
         // ~/breeze/todos/SaveChanges
         [HttpPost]
-        public SaveResult SaveChanges(JObject saveBundle) {
+        public SaveResult SaveChanges(JObject saveBundle)
+        {
             return _contextProvider.SaveChanges(saveBundle);
         }
 
@@ -57,25 +50,6 @@ namespace Todo.Controllers {
             Purge();
             TodoDatabaseInitializer.SeedDatabase(_contextProvider.Context);
             return "reset";
-        }
-
-        /// <summary>
-        /// Reset the database to it's initial data state after the server has run 
-        /// for "RefreshRate" minutes.
-        /// </summary>
-        private void PeriodicReset()
-        {
-            if ((DateTime.Now - _lastRefresh) > RefreshRate)
-            {
-                lock (Locker)
-                {
-                    if ((DateTime.Now - _lastRefresh) > RefreshRate)
-                    {
-                        _lastRefresh = DateTime.Now;
-                        Reset();
-                    }
-                }
-            }
         }
     }
 }
