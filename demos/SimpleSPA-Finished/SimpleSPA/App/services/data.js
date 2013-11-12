8a863028dec642a010a6df2a72f03729a113ecf6
Adam Barney <adam@Heisenberg.local>
2013-11-12 11:42:53 -0600

    Merge branch 'Start'

---
+++ demos/SimpleSPA-Finished/SimpleSPA/App/services/data.js
@@ -0,0 +1,15 @@
+﻿define(function () {
+        var dataService = {
+            getLibraries: getLibraries
+        };
+        return dataService;
+
+        function getLibraries() {
+            var options = {
+                url: 'api/javascriptlibs',
+                type: 'GET',
+                datatype: 'json'
+            };
+            return $.ajax(options);
+        };
+    });
---
+++ demos/SimpleSPA-Finished/SimpleSPA/Controllers/JavaScriptLibsController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SimpleSPA.Controllers
+{
+    public class JavaScriptLibsController : ApiController
+    {
+        public List<JavaScriptLib> GetJavaScriptLibs()
+        {
+            return new List<JavaScriptLib>
+            {
+                new JavaScriptLib{Id="1", Name="require.js", Purpose="Dependency Injection, modularization"},
+                new JavaScriptLib{Id="2", Name="knockout.js", Purpose="Data binding"},
+                new JavaScriptLib{Id="3", Name="jQuery", Purpose="DOM Manipulation"},
+                new JavaScriptLib{Id="4", Name="breeze.js", Purpose="entity management on the client"},
+                new JavaScriptLib{Id="5", Name="underscore", Purpose="LINQ-like operations in JS"},
+                new JavaScriptLib{Id="6", Name="Durandal", Purpose="Brings it all together - application framework"},
+                new JavaScriptLib{Id="7", Name="AngularJS", Purpose="Another great SPA option"},
+                new JavaScriptLib{Id="8", Name="Toastr", Purpose="Toast pop-ups"},
+                new JavaScriptLib{Id="9", Name="SignalR", Purpose="real-time pub-sub communication"},
+                new JavaScriptLib{Id="10", Name="Q", Purpose="Promises"},
+            };
+        }
+
+        public JavaScriptLib CeaterJavaScriptLib(JavaScriptLib lib)
+        {
+            lib.Id = Guid.NewGuid().ToString();
+            return lib;
+        }
+    }
+
+    public class JavaScriptLib
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Purpose { get; set; }
+    }
+}
