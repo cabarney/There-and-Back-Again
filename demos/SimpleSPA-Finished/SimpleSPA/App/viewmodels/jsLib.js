a0c7ef1b05b61fe709b69dffb00f538fdfb1908d
Adam Barney <adam@Heisenberg.local>
2013-11-12 11:33:52 -0600

    v2

---
+++ demos/SimpleSPA-Finished/SimpleSPA/App/viewmodels/jsLib.js
@@ -0,0 +1,23 @@
+﻿//TODO: Inject dependencies
+define(['plugins/router', 'services/data'],
+    function (router, data) {
+        var vm = {
+            activate: activate,
+            goBack: goBack,
+            title: 'JavaScript Libraries',
+            libraries: ko.observableArray()
+        };
+
+        return vm;
+        
+        function activate (id, querystring) {
+            var that = this;
+            data.getLibraries().done(function(results) {
+                that.libraries(results);
+            });
+        }
+
+        function goBack(complete) {
+            router.navigateBack();
+        }
+    });
